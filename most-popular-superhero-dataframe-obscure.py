2763a8beb290347b444291d2ad52037818dc9e98
diccode <dickymuhrizal@gmail.com>
2023-09-13 11:24:11 +0700

    add superhero joining

---
+++ most-popular-superhero-dataframe-obscure.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+spark = SparkSession.builder.appName("MostPopularSuperhero").getOrCreate()
+
+schema = StructType([ \
+                     StructField("id", IntegerType(), True), \
+                     StructField("name", StringType(), True)])
+
+names = spark.read.schema(schema).option("sep", " ").csv("Marvel-Names")
+
+lines = spark.read.text("Marvel-Graph")
+
+# Small tweak vs. what's shown in the video: we trim each line of whitespace as that could
+# throw off the counts.
+connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0]) \
+    .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
+    .groupBy("id").agg(func.sum("connections").alias("connections"))
+    
+obscure =  connections.sort(func.col("connections").asc()).first()
+
+obscureName = names.filter(func.col("id") == obscure[0]).select("name").first()
+
+print(f"Most obscure hero is {obscureName[0]} with only {obscure[1]} co-appereance")
